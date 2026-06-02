07652de40e688faba436b67540ac696bee816fc5
dimych <kuzyuberdin@gmail.com>
2026-06-02 18:03:53 +0400

    extra example for week2

---
+++ week-02/extra/different-networks/service2/app.js
@@ -0,0 +1,16 @@
+const http = require('http');
+
+const NAME = 'service2';
+const TARGET = 'http://service1:3000/';
+
+http.createServer((req, res) => {
+  res.end(JSON.stringify({ service: NAME, time: new Date().toISOString() }));
+}).listen(3000, () => console.log(`[${NAME}] listening on :3000, target=${TARGET}`));
+
+setInterval(() => {
+  http.get(TARGET, (res) => {
+    let body = '';
+    res.on('data', (c) => (body += c));
+    res.on('end', () => console.log(`[${NAME}] PING OK   -> ${TARGET} ${res.statusCode} ${body}`));
+  }).on('error', (e) => console.log(`[${NAME}] PING FAIL -> ${TARGET} ${e.code || e.message}`));
+}, 10000);
